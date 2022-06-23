e4b5a77fd9ad714b48a114a4af3fcacca46d35e7
plutos28 <gamevictor28@gmail.com>
2022-06-23 19:19:42 +0300

    make the move over to canvas based rendering

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const lastLeftNodeCoords = {x: 0, y: 0};
 const lastRightNodeCoords = {x: 0, y: 0};
 
 let rootNode = null;
+const canvas = document.querySelector("#canvas");
 
 // UI interactions - add new node
 addNodeButton.addEventListener("click", () => {
@@ -42,4 +43,57 @@ addNodeButton.addEventListener("click", () => {
         }
     }
     console.log(rootNode);
+    displayTree();
 });
+
+function displayTree() {
+    if (!canvas.getContext) {
+        return;
+    }
+    const ctx = canvas.getContext('2d');
+
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+
+    // draw a node
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    // add the text
+    ctx.font = '16px sans-serif';
+    ctx.fillText(`${rootNode.value}`, 100-12, 50+28);
+
+
+    // remove all node elements and recreate them and then render
+
+    let currentNode = rootNode;
+    while(currentNode != null) {
+        // display the node's value
+        // let parentNode = document.createElement("div");
+        // parentNode.textContent = currentNode.value;
+        // parentNode.classList.add("node");
+
+        // display the left nodes
+        // if(currentNode.leftNode != null) {
+        //     let leftNode = document.createElement("div");
+        //     leftNode.textContent = currentNode.leftNode.value;
+        //     leftNode.classList.add("node");
+        //     parentNode.appendChild(leftNode);
+        // }
+        
+    
+        // display the right nodes
+        // if(currentNode.rightNode != null) {
+        //     let rightNode = document.createElement("div");
+        //     rightNode.textContent = currentNode.rightNode.value;
+        //     rightNode.classList.add("node");
+        //     parentNode.appendChild(rightNode);
+        // }
+        
+
+        // tree.appendChild(parentNode);
+
+        break;
+    }
+}
