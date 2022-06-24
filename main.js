8c0b5b6d3ee5cded8aa4e1c6fd4e99f3389a0a1e
plutos28 <gamevictor28@gmail.com>
2022-06-24 17:08:19 +0300

    remove canvas stuff and add somebody's binary tree in d3

--- main.js
+++ main.js
@@ -13,18 +13,15 @@ const lastLeftNodeCoords = {x: 0, y: 0};
 const lastRightNodeCoords = {x: 0, y: 0};
 
 let rootNode = null;
-const canvas = document.querySelector("#canvas");
 
 // UI interactions - add new node
 addNodeButton.addEventListener("click", () => {
     let newNode = new Node(+addNodeText.value)
     console.log(newNode.value);
 
-    // create a new node and set it as the rootNode and/or add it as a child of currentNode
     if(rootNode === null) {
         rootNode = newNode;
     } else {
-        // loop through the tree to insert new element
         let currentNode = rootNode;
         while(currentNode.value != newNode.value) { 
             if(newNode.value < currentNode.value) {
@@ -47,53 +44,96 @@ addNodeButton.addEventListener("click", () => {
 });
 
 function displayTree() {
-    if (!canvas.getContext) {
-        return;
-    }
-    const ctx = canvas.getContext('2d');
-
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
-
-    // draw a node
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    // add the text
-    ctx.font = '16px sans-serif';
-    ctx.fillText(`${rootNode.value}`, 100-12, 50+28);
-
-
-    // remove all node elements and recreate them and then render
-
-    let currentNode = rootNode;
-    while(currentNode != null) {
-        // display the node's value
-        // let parentNode = document.createElement("div");
-        // parentNode.textContent = currentNode.value;
-        // parentNode.classList.add("node");
-
-        // display the left nodes
-        // if(currentNode.leftNode != null) {
-        //     let leftNode = document.createElement("div");
-        //     leftNode.textContent = currentNode.leftNode.value;
-        //     leftNode.classList.add("node");
-        //     parentNode.appendChild(leftNode);
-        // }
+    // I will be generating this data structure 
+    let treeData = [
+        {
+          "name": "Top Level",
+          "parent": "null",
+          "children": [
+            {
+              "name": "Level 2: A",
+              "parent": "Top Level",
+              "children": [
+                {
+                  "name": "Son of A",
+                  "parent": "Level 2: A"
+                },
+                {
+                  "name": "Daughter of A",
+                  "parent": "Level 2: A"
+                }
+              ]
+            },
+            {
+              "name": "Level 2: B",
+              "parent": "Top Level"
+            }
+          ]
+        }
+      ];
+      
+      // ************** Generate the tree diagram	 *****************
+      let margin = {top: 40, right: 120, bottom: 20, left: 120},
+          width = 960 - margin.right - margin.left,
+          height = 500 - margin.top - margin.bottom;
+          
+      let i = 0;
+      
+      let tree = d3.layout.tree()
+          .size([height, width]);
+      
+      let diagonal = d3.svg.diagonal()
+          .projection(function(d) { return [d.x, d.y]; });
+      
+      let svg = d3.select("body").append("svg")
+          .attr("width", width + margin.right + margin.left)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      
+      root = treeData[0];
         
-    
-        // display the right nodes
-        // if(currentNode.rightNode != null) {
-        //     let rightNode = document.createElement("div");
-        //     rightNode.textContent = currentNode.rightNode.value;
-        //     rightNode.classList.add("node");
-        //     parentNode.appendChild(rightNode);
-        // }
-        
-
-        // tree.appendChild(parentNode);
-
-        break;
-    }
+      update(root);
+      
+      function update(source) {
+      
+        // Compute the new tree layout.
+        let nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
+      
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { d.y = d.depth * 100; });
+      
+        // Declare the nodes…
+        let node = svg.selectAll("g.node")
+            .data(nodes, function(d) { return d.id || (d.id = ++i); });
+      
+        // Enter the nodes.
+        let nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { 
+                return "translate(" + d.x + "," + d.y + ")"; });
+      
+        nodeEnter.append("circle")
+            .attr("r", 10)
+            .style("fill", "#fff");
+      
+        nodeEnter.append("text")
+            .attr("y", function(d) { 
+                return d.children || d._children ? -18 : 18; })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .text(function(d) { return d.name; })
+            .style("fill-opacity", 1);
+      
+        // Declare the links…
+        let link = svg.selectAll("path.link")
+            .data(links, function(d) { return d.target.id; });
+      
+        // Enter the links.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", diagonal);
+      
+      }
 }
